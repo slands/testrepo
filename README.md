135a427634086edccdf3f59cd6ca711a725aa2d8
slands <stephen.shortland@gmail.com>
2019-06-12 19:50:34 +0000

    added glah

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Just a test
 Added just a test
 
 Update README
+
+# A new header
+the quick brown fox jumps over the lazy dog. Updated loop counter
--- test.pl
+++ test.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl
 
-for my $i (0..100) {
-    print "Hello $i\n"
+for my $i (0..10) {
+    print "Hello $i\n";
+    blah($i);
 }
 
+sub blah {
+    my $j = shift;
+    print "blah says $j\n"
+}
